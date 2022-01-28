acb93b34cec83d72a984391cb5b48c5e7b5e4681
Terrys101 <97230300+Terrys101@users.noreply.github.com>
2022-01-28 16:22:07 +0000

    Add files via upload

---
+++ week2/monday17.js
@@ -0,0 +1,36 @@
+// const pet = {
+//     name: "max",
+//     typeOfPet: "terrier",
+//     age: 1,
+//     colour: "black",
+//    eat: function () {
+       
+//        console.log(this.name + " is eating")
+//    },
+//    drink: function () {
+//     console.log(this.name + " is drinking");
+//    },
+//    age: function () {
+    
+       
+//    }
+// };
+
+// pet.eat();
+// pet.drink();
+
+
+// const shoppingList = ["Bakery and Bread", "Meat and Seafood","Pasta and Rice"];
+// console.log(shoppingList)
+
+
+// let array = ["something", "something else"];
+// let multiArray = [["something", "something else"], [" another things", "running out of ideas"]];
+
+// let bigOne = [
+//     {cat: "veg", list: ["carrot", "peppers"]}, 
+//     {cat: "dog", list: ["food", "chews"]}, 
+//     {cat: "cleaning", list: ["sponges", "fairy liquid"]}
+// ];
+
+// console.log(bigOne[1].list[1]);
